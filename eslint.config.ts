d8e68c9b39b603163ab9679bca6b630449ee84f9
jakubserwin <kuba.serwin99@gmail.com>
2024-03-17 20:48:18 +0100

    feat: tailwind, unused-imports, arrow fn

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,28 @@
+import { FlatCompat } from '@eslint/eslintrc'
 import jsConfig from './src'
 
-export default jsConfig()
+const compat = new FlatCompat()
+
+export default jsConfig(
+  {},
+  ...compat.config({
+    plugins: ['unused-imports', 'prefer-arrow-functions'],
+    extends: ['plugin:tailwindcss/recommended'],
+    rules: {
+      'unused-imports/no-unused-imports': 'error',
+      
+      'tailwindcss/no-custom-classname': 'off',
+      
+      'prefer-arrow-functions/prefer-arrow-functions': [
+        'error',
+        {
+          'allowNamedFunctions': false,
+          'classPropertiesAllowed': false,
+          'disallowPrototype': false,
+          'returnStyle': 'unchanged',
+          'singleReturnOnly': false
+        }
+      ]
+    },
+  })
+)
